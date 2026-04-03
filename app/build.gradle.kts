c66e67aacb1b9f26a16cd7f0eebdd0d8c270b103
fahimshahriarv1 <f.sm151496@gmail.com>
2026-04-03 15:55:43 +0600

    lib versions changed and modified.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
 
     // Hilt
     implementation(libs.hilt.android)
+    implementation(libs.androidx.navigation.runtime.ktx)
     ksp(libs.hilt.compiler)
     implementation(libs.hilt.navigation.compose)
 
